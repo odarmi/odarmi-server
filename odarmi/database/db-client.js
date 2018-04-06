a4e28dcd7f4fa3e1b575965e78a622f12b1cc67b
Ben Fu <ben_fu1@yahoo.com>
2018-04-06 16:53:04 -0500

    Add knex and pg interface

--- odarmi/database/db-client.js
+++ odarmi/database/db-client.js
@@ -1,10 +1,14 @@
 
-class DbClient {
-    constructor() {
-        this.name = "happy";
+const Knex = require("knex")
+pg = new Knex({
+    client: "pg",
+    version: "9.6",
+    connection: {
+        host: "ben-db.cpbikfhxnqpa.us-east-1.rds.amazonaws.com",
+        user: "postgres",
+        password: "postgres",
+        database: "odarmi"
     }
+})
 
-
-}
-
-module.exports = DbClient;
+module.exports = pg;
--- odarmi/index.js
+++ odarmi/index.js
@@ -1,7 +1,23 @@
 
 const Koa = require("koa");
-const DbClient = require("odarmi/database/db-client")
+let pg = require("odarmi/database/db-client");
 
-let app = new Koa();
-let dbClient = new DbClient();
-console.log(dbClient.name);
+async function main() {
+    let app = new Koa();
+    
+    let exists = await pg.schema.hasTable("users");
+    console.log(exists);
+    if (!exists) {
+        let res = await pg.schema.createTable("users", (table) => {
+            table.increments().primary();
+            table.string("email").unique();
+        });
+    }
+    
+    res = await pg("users").insert({email: "aytung94@gmail.com"});
+    console.log(res);
+
+    app.listen(3000);
+}
+
+main()
